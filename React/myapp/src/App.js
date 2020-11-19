858dc3074ed1751d40350e99b78ef07a04e7ebc9
mahfuj-m <18.1mahfuj@gmail.com>
2020-11-19 22:49:34 +0600

    addressing the functionalities

---
+++ React/myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div>
+<h1> Mahfujur Rahman</h1>
+    <h2>Learning Components</h2>
+</div>
+
+  );
+}
+
+export default App;
