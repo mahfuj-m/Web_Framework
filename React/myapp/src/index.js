b4db874463489c72dce7220d81723b0f3c3580b6
mahfuj-m <18.1mahfuj@gmail.com>
2020-11-20 23:14:21 +0600

    Sign Up System added

--- React/myapp/src/index.js
+++ React/myapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './MusicComp/App/App';
+import App from './Verification/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
